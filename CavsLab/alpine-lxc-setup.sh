3755b09469e495c1384762bd369fc7caa10438ca
Blake Seufert <blakeseufert@users.noreply.github.com>
2025-10-31 11:34:01 +1100

    Create alpine-lxc-setup.sh

---
+++ CavsLab/alpine-lxc-setup.sh
@@ -0,0 +1,63 @@
+#!/bin/sh
+
+YELLOW='\e[33m'
+NC='\e[0m'
+
+echo -e ${YELLOW}"
+   █████████                                █████                 █████    
+  ███▒▒▒▒▒███                              ▒▒███                 ▒▒███     
+ ███     ▒▒▒   ██████   █████ █████  █████  ▒███         ██████   ▒███████ 
+▒███          ▒▒▒▒▒███ ▒▒███ ▒▒███  ███▒▒   ▒███        ▒▒▒▒▒███  ▒███▒▒███
+▒███           ███████  ▒███  ▒███ ▒▒█████  ▒███         ███████  ▒███ ▒███
+▒▒███     ███ ███▒▒███  ▒▒███ ███   ▒▒▒▒███ ▒███      █ ███▒▒███  ▒███ ▒███
+ ▒▒█████████ ▒▒████████  ▒▒█████    ██████  ███████████▒▒████████ ████████ 
+  ▒▒▒▒▒▒▒▒▒   ▒▒▒▒▒▒▒▒    ▒▒▒▒▒    ▒▒▒▒▒▒  ▒▒▒▒▒▒▒▒▒▒▒  ▒▒▒▒▒▒▒▒ ▒▒▒▒▒▒▒▒  
+
+${NC}"
+echo -e  "${YELLOW}--- Alpine LXC setup script. Includes Docker, Tailscale & Utils ---${NC}" 
+echo -e "${YELLOW}------ Connects to cephfs share /dockerdata/${NC}"                                                                          
+                                                                           
+# Update package lists and install all required utilities and services.
+echo -e "${YELLOW}Updating package list and installing packages...${NC}" 
+apk update
+apk add docker || true
+apk add tailscale || true
+apk add btop || true
+apk add nano || true 
+apk add openssh || true
+apk add util-linux || true 
+apk add curl || true 
+apk add docker-cli-compose || true
+apk add neofetch || true
+
+# ---
+# Configure Services to Start at Boot (OpenRC)
+# ---
+
+echo -e "${YELLOW}Enabling services to start at boot (docker, tailscale, sshd)...${NC}" 
+
+# 1. Enable and start the Docker service
+rc-update add docker default
+rc-service docker start
+
+# 2. Enable and start the SSH daemon
+rc-update add sshd default
+rc-service sshd start
+
+# 3. Enable the Tailscale service to run at boot
+rc-update add tailscale default
+rc-service tailscale start
+
+# This Tailscale setup process.
+echo -e "${YELLOW}Setup Tailscale tun userspace-networking...${NC}" 
+/usr/sbin/tailscaled --tun=userspace-networking
+echo -e "${YELLOW}Starting Tailscale interactive setup...${NC}" 
+tailscale up
+
+echo -e "${YELLOW} Remember to: 
+- Connect Tailscale (above URL)
+- Connect cephfs share as a mountpoint for the LXC on proxmox
+  - pct set [[CTID eg:100]] -mp0 /mnt/pve/cephfs/docker,mp=/dockerdata,shared=1
+- Reboot${NC}"
+
+sleep 1
